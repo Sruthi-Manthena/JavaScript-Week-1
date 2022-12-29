af8713e37dc10508f03c91d43e54b48ac8b2299d
Sruthi-Manthena <84638439+Sruthi-Manthena@users.noreply.github.com>
2022-12-29 12:05:16 +0530

    Update loops.js

--- loops.js
+++ loops.js
@@ -69,13 +69,3 @@ module.exports = topDouble;
 
 
 
-//Given an integer value num, determine if it is even. If it is even, return true. Return false otherwise.
-function isEven(num) {
-    if(num%2===0){
-        return true;
-    }
-    else{
-        return false;
-    }   
-}
-module.exports = isEven;
